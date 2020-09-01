2ccd74a7a2ad6d00c8a78d1dc0c8442611dfb222
Mikolaj Zywczok <mikolaj.zywczok@gmail.com>
2020-09-01 11:08:57 +0200

    organize code with modules and namespaces

---
+++ src/components/project-item.ts
@@ -0,0 +1,50 @@
+/// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../models/drag-drop.ts" />
+/// <reference path="../models/project.ts" />
+
+// NOTE ProjectItem Class
+namespace App {
+  export class ProjectItem
+    extends Component<HTMLUListElement, HTMLLIElement>
+    implements Draggable {
+    private project: Project;
+
+    get persons() {
+      return this.project.people === 1
+        ? '1 person'
+        : `${this.project.people} persons`;
+    }
+
+    constructor(hostId: string, project: Project) {
+      super('single-project', hostId, false, project.id);
+      this.project = project;
+
+      this.configure();
+      this.renderContent();
+    }
+
+    @autobind
+    dragStartHandler(event: DragEvent) {
+      event.dataTransfer!.setData('text/plain', this.project.id);
+      event.dataTransfer!.effectAllowed = 'move';
+    }
+
+    dragEndHandler(_: DragEvent) {
+      console.log('Drag ended!');
+    }
+
+    configure() {
+      this.element.addEventListener('dragstart', this.dragStartHandler);
+      this.element.addEventListener('dragend', this.dragEndHandler);
+    }
+
+    renderContent() {
+      this.element.querySelector('h2')!.textContent = this.project.title;
+      this.element.querySelector(
+        'h3',
+      )!.textContent = `${this.persons} assigned`;
+      this.element.querySelector('p')!.textContent = this.project.description;
+    }
+  }
+}
